2385cfa4a3d92b530fcddec066aab7ce8743a076
mark.otting <mark.otting@vopak.com>
2021-05-23 01:20:09 +0200

    Refactored vote validation Added switch for self-voting

--- psychopomp.py
+++ psychopomp.py
@@ -1,8 +1,7 @@
 # bot.py
 import os
-import re
 
-from votes import Votes
+from votes import Votes, MessageValidator, MessageValidationException
 import discord
 from dotenv import load_dotenv
 
@@ -13,7 +12,6 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 votes = Votes()
-message_expr = re.compile(os.environ["VOTE_TRIGGER"])
 
 # These will be set in the startup function
 standing_channelid = False
@@ -66,42 +64,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message_expr.match(message.content.lower()):
-        if len(message.mentions) == 0:
-            if "@" in message.content:
-                await message.reply(
-                    "Oh, hoogster aller machten, het spijt mij, ik herken de naam niet die u gebruikt na uw apestaart teken.")
-            else:
-                await message.reply("Oh, hoogster aller machten, ik herinner u graag dat u stemmen kunt uitbrengen "
-                                    "door te verwijzen naar de naam van uw stem met voorgaand een @. U zou "
-                                    "bijvoorbeeld @Psychopomp kunnen gebruiken, hoewel Pavi mij direct zou "
-                                    "neerbliksemen al zou ik die stem aannemen van u.")
-        elif len(message.mentions) > 1:
-            await message.reply("Oh, hoogster aller machten, hoe gul u ook bent, ik herinner u graag dat u slecht op "
-                                "één persoon kunt stemmen")
-        elif message.mentions[0] == client.user:
-            await message.reply("Oh, hoogster aller machten, hoe gul u ook bent, ik weiger uw stem en hoop zo de "
-                                "toorn van mijn meester Pavi te vermijden")
-        else:
-            try:
-                votee = message.mentions[0]
+    voter = get_member_for_user(message.author)
 
-                # Private messages are outside of the context of a server, so we get a "user"  and not a "member"
-                if type(votee) == discord.user.User:
-                    votee = get_member_for_user(votee)
+    ms = MessageValidator(message, voter)
+    if ms.is_voting_message():
+        try:
+            votee = ms.get_votee_from_message()
+            # Private messages are outside of the context of a server, so we get a "user" and not a "member"
+            if type(votee) == discord.user.User:
+                votee = get_member_for_user(votee)
 
-                if message.guild and os.environ["PUBLIC_VOTING"] != "true":
-                    await message.reply("Ik kan uw stem niet aannemen, ik verzoek u vriendelijk om uw stemmen direct aan mij te sturen in een privé bericht" )
-                elif os.environ["PRIVATE_VOTING"] != "true":
-                    await message.reply("Ik kan uw stem niet aannemen, ik verzoek u vriendelijk om uw stem openbaar te plaatsen")
-                else:
-                    await message.reply(f"Ik noteer uw stem op {votee.display_name}")
-                    votes.vote_for(get_member_for_user(message.author), votee)
-                    await publish_standings()
-            except Exception as e:
-                print(e)
-                await message.reply(f"Oh nee! Er ging iets mis in het verwerken van uw stem. Hij is niet aangenomen, ik ben even aan het kijken wat er aan de hand is!")
+            await message.reply(f"Ik noteer uw stem op {votee.display_name}")
+            votes.vote_for(voter, votee)
+            await publish_standings()
 
+        except MessageValidationException as e:
+            await message.reply(str(e))
 
 client.run(os.getenv('DISCORD_TOKEN'))
 
--- votes.py
+++ votes.py
@@ -1,5 +1,50 @@
-from discord import member
+import os
+import re
+
+import discord
+from discord import member, message
 import datetime
+
+
+class MessageValidationException(Exception):
+    pass
+
+
+class MessageValidator:
+
+    def __init__(self, message, sender):
+        self.message_expr = re.compile(os.environ["VOTE_TRIGGER"])
+        self.sender = sender
+        self.message = message
+
+    def is_voting_message(self):
+        return self.message_expr.match(self.message.content.lower())
+
+    def get_votee_from_message(self):
+        if len(self.message.mentions) == 0:
+            if "@" in self.message.content:
+                raise MessageValidationException("Oh, hoogster aller machten, het spijt mij, ik herken de naam niet die u gebruikt na uw apestaart teken.")
+            else:
+                raise MessageValidationException("Oh, hoogster aller machten, ik herinner u graag dat u stemmen kunt uitbrengen "
+                                    "door te verwijzen naar de naam van uw stem met voorgaand een @. U zou "
+                                    "bijvoorbeeld @Psychopomp kunnen gebruiken, hoewel Pavi mij direct zou "
+                                    "neerbliksemen al zou ik die stem aannemen van u.")
+        elif len(self.message.mentions) > 1:
+            raise MessageValidationException("Oh, hoogster aller machten, hoe gul u ook bent, ik herinner u graag dat u slecht op "
+                                "één persoon kunt stemmen")
+        elif self.message.mentions[0] == self.sender and os.environ["CAN_VOTE_SELF"] != "true":
+            raise MessageValidationException("Het spijt mij. De machten hebben bepaald dat u niet op zichzelf kunt stemmen")
+
+        if self.message.guild and os.environ["PUBLIC_VOTING"] != "true":
+            raise MessageValidationException(
+                "Ik kan uw stem niet aannemen, ik verzoek u vriendelijk om uw stemmen direct aan mij te sturen in een privé bericht")
+
+        if not self.message.guild and os.environ["PRIVATE_VOTING"] != "true":
+            raise MessageValidationException(
+                "Ik kan uw stem niet privé aannemen, ik verzoek u vriendelijk om uw stem openbaar te plaatsen")
+
+        votee = self.message.mentions[0]
+        return votee
 
 
 class Vote:
